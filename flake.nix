366d0fccda56eb6c122d81e58ce030cc92509dd8
wucke13 <wucke13@gmail.com>
2021-07-29 15:24:27 +0200

    bump versions

--- flake.nix
+++ flake.nix
@@ -2,33 +2,45 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk}:
-  utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ] (system:
-  let
-    pkgs = nixpkgs.legacyPackages."${system}";
-    naersk-lib = naersk.lib."${system}";
-  in rec {
-    packages.opentaws = naersk-lib.buildPackage {
-      pname = "opentaws";
-      root = ./.;
-      doCheck = true;
-      doDoc = true;
-      copyLibs = true;
-      doDocFail = true;
-      copyTarget = true;
-    };
-    defaultPackage = packages.opentaws;
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
+    utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
+    (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        naersk-lib = naersk.lib."${system}";
+      in rec {
+        packages.opentaws = naersk-lib.buildPackage {
+          pname = "opentaws";
+          root = ./.;
+          doCheck = true;
+          doDoc = true;
+          copyLibs = true;
+          doDocFail = true;
+          copyTarget = true;
+        };
+        defaultPackage = packages.opentaws;
 
-    devShell = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [ rustc cargo ];
-    };
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              (rust-bin.stable.latest.default.override {
+                extensions =
+                  [ "rust-src" "clippy" "rustfmt" "llvm-tools-preview" ];
+                targets = [ "arm-unknown-linux-gnueabihf" ];
+              })
+            ];
+        };
 
-    # Hail to the Hydra
-    hydraJobs.opentaws."system" = packages.opentaws // { meta = {
-      timeout = 86400;
-      maxSilent = 36000;
-    };};
-  });
+        # Hail to the Hydra
+        hydraJobs.opentaws."system" = packages.opentaws // {
+          meta = {
+            timeout = 86400;
+            maxSilent = 36000;
+          };
+        };
+      });
 }
