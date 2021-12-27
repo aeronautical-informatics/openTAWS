9a84132834c7038caefa1c9cd4842f787375d04b
autumnal <friedrich122112@me.com>
2021-12-27 10:43:47 +0100

    [WIP] 1 Point elevation

--- terrain_database/src/database.rs
+++ terrain_database/src/database.rs
@@ -1,17 +1,24 @@
-use geo::Point;
+use crate::prediction::HeightPrediction;
 use geo::prelude::*;
+use geo::Point;
+use rayon::prelude::*;
+use taws_primitives::Position;
 use uom::si::angle::degree;
 use uom::si::f64::{Angle, Length, Time};
 use uom::si::length::{foot, meter};
 use uom::si::ratio::ratio;
-use taws_primitives::Position;
-use crate::prediction::HeightPrediction;
-use rayon::prelude::*;
 
 pub trait TerrainDatabase {
     /// Estimate elevation at a specific position
     fn elevation_at(&self, long: Angle, lat: Angle) -> Option<Length>;
-    fn clearance(&self, prediction: HeightPrediction, start: Position, bearing: Angle, check_duration: Time, resolution: Length) -> Option<Length>;
+    fn clearance(
+        &self,
+        prediction: HeightPrediction,
+        start: Position,
+        bearing: Angle,
+        check_duration: Time,
+        resolution: Length,
+    ) -> Option<Length>;
 }
 
 pub struct SimpleTerrainDatabase<const SIZE_LONG: usize, const SIZE_LAT: usize> {
@@ -37,39 +44,54 @@ impl<const SIZE_LONG: usize, const SIZE_LAT: usize> TerrainDatabase
             Angle::new::<degree>(self.step.lat()),
         );
 
-        let index_long: [i64; 2] = {
-            let index = ((input.0 - start.0) / step.0).get::<ratio>();
-            [index.floor() as i64, index.ceil() as i64]
-        };
-        let index_lat: [i64; 2] = {
-            let index = ((input.1 - start.1) / step.1).get::<ratio>();
-            [index.floor() as i64, index.ceil() as i64]
-        };
+        let index_long = ((input.0 - start.0) / step.0).get::<ratio>().round() as i64;
+        let index_lat = ((input.1 - start.1) / step.1).get::<ratio>().round() as i64;
 
-        let mut max = None;
-        for i_long in index_long {
-            for i_lat in index_lat {
-                if i_long >= 0 && i_lat >= 0 {
-                    max = max.max(
-                        self.data
-                            .get(i_long as usize)
-                            .map(|l| l.get(i_lat as usize))
-                            .flatten(),
-                    )
-                }
-            }
+        if index_long < 0 || index_lat < 0 {
+            return None;
         }
 
-        max.map(|m| Length::new::<foot>(*m as f64))
+        self.data
+            .get(index_long as usize)
+            .map(|l| l.get(index_lat as usize))
+            .flatten()
+            .map(|m| Length::new::<foot>(*m as f64))
     }
 
-    fn clearance(&self, prediction: HeightPrediction, start: Position, bearing: Angle, check_duration: Time, resolution: Length) -> Option<Length> {
+    fn clearance(
+        &self,
+        prediction: HeightPrediction,
+        start: Position,
+        bearing: Angle,
+        check_duration: Time,
+        resolution: Length,
+    ) -> Option<Length> {
         let start = Point::new(start.lon.get::<degree>(), start.lat.get::<degree>());
-        let end = start.haversine_destination(bearing.get::<degree>(), prediction.distance_by_time(check_duration).get::<meter>());
+        let end = start.haversine_destination(
+            bearing.get::<degree>(),
+            prediction.distance_by_time(check_duration).get::<meter>(),
+        );
 
         let points = start.haversine_intermediate_fill(&end, resolution.get::<meter>(), true);
-        points.par_iter()
-            .filter_map(|p| self.elevation_at(Angle::new::<degree>(p.lng()), Angle::new::<degree>(p.lat())))
+        points
+            //Iterate over all points in parallel
+            .par_iter()
+            // Map List other element but filter out "None" Values
+            // "None" Values are only possible if our airplane exits the area covered by our terrain database
+            .filter_map(|p| {
+                // Calculate clearance by getting the elevation at this point
+                self.elevation_at(Angle::new::<degree>(p.lng()), Angle::new::<degree>(p.lat()))
+                    // If Height at this point exists
+                    .map(|height| {
+                        // Subtract height from predicted airplane height
+                        prediction
+                            // Get Height of Airplane by calculating the horizontal distance of our start point to our current point
+                            // Then get the predicted height by asking our prediction function for the height at this distance
+                            .height_by_distance(Length::new::<meter>(start.haversine_distance(p)))
+                            - height
+                    })
+            })
+            // Filter so only finite values exist (infinite [and NaN] values are only possible if the elevation database is broken)
             .filter(|c| c.is_finite())
             .min_by(|x, y| x.partial_cmp(y).unwrap())
     }
