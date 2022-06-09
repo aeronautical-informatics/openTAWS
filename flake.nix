0024ceee43ec78f52c00cebd1a69cd4f518f0881
Wanja Zaeske <wanja.zaeske@dlr.de>
2022-06-09 15:21:11 +0200

    update deps and flake.nix

--- flake.nix
+++ flake.nix
@@ -1,46 +1,54 @@
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    utils.url = "git+https://github.com/numtide/flake-utils.git";
+    fenix = {
+      url = "git+https://github.com/nix-community/fenix.git?ref=main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "git+https://github.com/nix-community/naersk.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
+  outputs = { self, nixpkgs, utils, fenix, naersk }:
     utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
-    (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        naersk-lib = naersk.lib."${system}";
-      in rec {
-        packages.opentaws = naersk-lib.buildPackage {
-          pname = "opentaws";
-          root = ./.;
-          doCheck = true;
-          doDoc = true;
-          copyLibs = true;
-          doDocFail = true;
-          copyTarget = true;
-        };
-        defaultPackage = packages.opentaws;
-
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs;
-            [
-              (rust-bin.stable.latest.default.override {
-                extensions =
-                  [ "rust-src" "clippy" "rustfmt" "llvm-tools-preview" ];
-                targets = [ "arm-unknown-linux-gnueabihf" ];
-              })
-            ];
-        };
-
-        # Hail to the Hydra
-        hydraJobs.opentaws."system" = packages.opentaws // {
-          meta = {
-            timeout = 86400;
-            maxSilent = 36000;
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          rust-toolchain = with fenix.packages.${system}; combine [
+            rust-analyzer
+            stable.cargo
+            stable.rustc
+            stable.rustfmt
+          ];
+          naersk-lib = (naersk.lib."${system}".override {
+            cargo = rust-toolchain;
+            rustc = rust-toolchain;
+          });
+        in
+        rec {
+          packages.opentaws = naersk-lib.buildPackage {
+            pname = "opentaws";
+            root = ./.;
+            doCheck = true;
+            doDoc = true;
+            copyLibs = true;
+            doDocFail = true;
+            copyTarget = true;
           };
-        };
-      });
+          defaultPackage = packages.opentaws;
+
+          devShell = pkgs.mkShell {
+            inputsFrom = [ packages.opentaws ];
+          };
+
+          # Hail to the Hydra
+          hydraJobs.opentaws."system" = packages.opentaws // {
+            meta = {
+              timeout = 86400;
+              maxSilent = 36000;
+            };
+          };
+        });
 }
