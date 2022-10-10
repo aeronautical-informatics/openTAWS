561d5648baf15fef9e50a2388cadd24c10928992
Moritz Meier <mam14@tu-clausthal.de>
2022-10-10 17:36:43 +0200

    rework opentaws implementation

--- opentaws/src/lib.rs
+++ opentaws/src/lib.rs
@@ -10,25 +10,73 @@
 
 #![no_std]
 #![deny(unsafe_code)]
+#![allow(dead_code)]
 
-pub use alerts::{functionalities, Alert, AlertLevel, AlertState};
-use prelude::*;
-pub use types::*;
+extern crate enum_map;
 
+#[cfg(test)]
 #[macro_use]
-pub mod macros;
+extern crate std;
 
-mod alerts;
+pub mod aircraft_state;
+pub mod alerts;
 mod envelope;
-pub mod prelude;
-mod types;
 
-pub trait Taws<const N: usize> {
-    fn is_armed(&self, alert_system: Alert) -> bool;
-    fn arm(&mut self, alert_system: Alert);
-    fn disarm(&mut self, alert_system: Alert);
-    fn is_inhibited(&self, alert_system: Alert) -> bool;
-    fn inhibit(&mut self, alert_system: Alert);
-    fn uninhibit(&mut self, alert_system: Alert);
-    fn process(&mut self, state: &AircraftState) -> AlertState<N>;
+pub use aircraft_state::{AircraftState, NormalizedAircraftState};
+pub use alerts::{Alert, AlertLevel, AlertSource};
+
+/// Abstraction for a TAWS system
+pub trait Taws {
+    /// Alert set type
+    type Alerts: TawsAlerts + Default;
+
+    //fn arm(&mut self, alert_src: AlertSource);
+    //fn disarm(&mut self, alert_src: AlertSource);
+
+    /// Returns whether the specified alert source (TAWS functionallity) is currently armed.
+    /// # Arguments
+    /// * `alert_src` - The alert source of which the armed state is returned.
+    fn is_armed(&self, alert_src: AlertSource) -> bool;
+
+    /// Inhibits the output of the specified alert source.
+    /// # Arguements
+    /// * `alert_src` - The alert source to inhibit.
+    fn inhibit(&mut self, alert_src: AlertSource);
+
+    /// Un-inhibits the output of the specified alert source.
+    /// # Arguements
+    /// * `alert_src` - The alert source to un-inhibit.
+    fn uninhibit(&mut self, alert_src: AlertSource);
+
+    /// Returns whether the specified alert source is currently inhibited.
+    /// # Arguments
+    /// * `alert_src` - The alert source of which the inhibit state is returned.
+    fn is_inhibited(&self, alert_src: AlertSource) -> bool;
+
+    /// Processes an normalized [AircraftState] and
+    /// returns an alert for each alert source if the related conditions for this TAWS functionallity are given.
+    /// # Arguments
+    /// * `state` - The normalized [AircraftState] to process.
+    fn process(&mut self, state: NormalizedAircraftState) -> Self::Alerts;
+}
+
+/// Abstraction for a TAWS alert
+pub trait TawsAlert: Into<(AlertSource, AlertLevel)> + Eq {
+    /// Returns the TAWS functionallity which emitted this alert
+    fn source(&self) -> AlertSource;
+
+    /// Returns the alert level of this alert
+    fn level(&self) -> AlertLevel;
+}
+
+/// Abstraction for a set of TAWS alerts
+pub trait TawsAlerts {
+    /// Alert type
+    type Alert: TawsAlert;
+
+    /// Returns whether an alert from the given source with at least the specified alert level is active.
+    /// # Arguments
+    /// * `alert_src` - The alert source (TAWS functionallity) to check for
+    /// * `min_level` - The inclusive min level to check for
+    fn is_alert_active(&self, alert_src: AlertSource, min_level: AlertLevel) -> bool;
 }
